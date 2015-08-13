a9afcd6b74d8f359737308c27f09b59c739ce090
Jay Zeschin <jay.zeschin@modeset.com>
2015-08-13 14:54:12 -0600

    Add an outer loop to the iterator to get a new ShardIterator

--- processor.rb
+++ processor.rb
@@ -19,41 +19,49 @@ resp = client.describe_stream(stream_name: stream_name, limit: 1)
 shard_id = resp.stream_description.shards[0].shard_id
 
 # Get an initial iterator
-iterator_opts = {
-  stream_name: stream_name,
-  shard_id: shard_id,
-}
-if last_sequence_number
-  iterator_opts[:shard_iterator_type] = 'AFTER_SEQUENCE_NUMBER'
-  iterator_opts[:starting_sequence_number] = last_sequence_number
-else
-  iterator_opts[:shard_iterator_type] = 'TRIM_HORIZON'
-end
-resp = client.get_shard_iterator(iterator_opts)
-shard_iterator = resp.shard_iterator
-
-
-# Iterate!
 loop do
-  resp = client.get_records({
-    shard_iterator: shard_iterator,
-    limit: 10,
-  })
-
-  resp.records.each do |record|
-    begin
-      file = StringIO.new(record.data)
-      reader = Avro::DataFile::Reader.new(file, Avro::IO::DatumReader.new)
-      reader.each do |avro_record|
-        event_name = reader.datum_reader.readers_schema.name
-        puts "[#{record.sequence_number}] #{event_name}: #{avro_record}"
-      end
-    rescue Avro::DataFile::DataFileError => e
-      puts "[#{record.sequence_number}] Unable to parse Avro record: #{e.inspect}"
+  begin
+    iterator_opts = {
+      stream_name: stream_name,
+      shard_id: shard_id,
+    }
+    if last_sequence_number
+      iterator_opts[:shard_iterator_type] = 'AFTER_SEQUENCE_NUMBER'
+      iterator_opts[:starting_sequence_number] = last_sequence_number
+    else
+      iterator_opts[:shard_iterator_type] = 'TRIM_HORIZON'
     end
-    last_sequence_number = record.sequence_number
-    Redis.current.set('drip-kinesis-last-seq', last_sequence_number)
-  end
+    resp = client.get_shard_iterator(iterator_opts)
+    shard_iterator = resp.shard_iterator
 
-  shard_iterator = resp.next_shard_iterator
+
+    # Iterate!
+    loop do
+      puts "Getting records for #{shard_iterator}"
+      resp = client.get_records({
+        shard_iterator: shard_iterator,
+        limit: 10,
+      })
+
+      resp.records.each do |record|
+        begin
+          file = StringIO.new(record.data)
+          reader = Avro::DataFile::Reader.new(file, Avro::IO::DatumReader.new)
+          reader.each do |avro_record|
+            event_name = reader.datum_reader.readers_schema.name
+            puts "[#{record.sequence_number}] #{event_name}: #{avro_record}"
+          end
+        rescue Avro::DataFile::DataFileError => e
+          puts "[#{record.sequence_number}] Unable to parse Avro record: #{e.inspect}"
+        end
+        last_sequence_number = record.sequence_number
+        Redis.current.set('drip-kinesis-last-seq', last_sequence_number)
+      end
+
+      shard_iterator = resp.next_shard_iterator
+    end
+
+  rescue Aws::Kinesis::Errors::ExpiredIteratorException
+    puts "Iterator expired! Fetching a new one."
+  end
 end
