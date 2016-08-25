5351dd4986f798d8059438b2cd772ba43d12d13b
Justin-Holmes <justin.ryan.holmes@icloud.com>
2016-08-25 16:36:54 -0600

    Add `Categories` as a group on the user’s list

--- lib/ello/mailchimp_wrapper.rb
+++ lib/ello/mailchimp_wrapper.rb
@@ -18,7 +18,7 @@ class MailchimpWrapper
     end
   end
 
-  def upsert_to_users_list(email, preferences)
+  def upsert_to_users_list(email, preferences, categories = [])
     return if skip_list.include?(email)
     hash = subscriber_hash(email)
     begin
@@ -26,22 +26,7 @@ class MailchimpWrapper
         body: {
           email_address: email,
           status_if_new: 'subscribed',
-          interests: prefs_to_interest_groups(preferences)
-        })
-    rescue Gibbon::MailChimpError => e
-      # Ideally this would be more specific, but they don't let us just check the e-mail field
-      raise e unless e.status_code == 400
-    end
-  end
-
-  def upsert_to_categories_list(email, preferences)
-    hash = subscriber_hash(email)
-    begin
-      categories_list.members(hash).upsert(
-        body: {
-          email_address: email,
-          status_if_new: 'subscribed',
-          interests: prefs_to_interest_groups_for_categories(preferences)
+          interests: prefs_to_interest_groups(preferences, categories)
         })
     rescue Gibbon::MailChimpError => e
       # Ideally this would be more specific, but they don't let us just check the e-mail field
@@ -59,15 +44,15 @@ class MailchimpWrapper
     gibbon.lists(ENV['MAILCHIMP_USERS_LIST_ID'])
   end
 
-  def categories_list
-    gibbon.lists(ENV['MAILCHIMP_CATEGORIES_LIST_ID'])
-  end
-
   def subscriber_hash(email)
     Digest::MD5.hexdigest(email.downcase)
   end
 
-  def prefs_to_interest_groups(prefs)
+  def prefs_to_interest_groups(prefs, categories)
+    prefs_to_newsletter_interest_group(prefs).merge(prefs_to_categories_interest_group(categories))
+  end
+
+  def prefs_to_newsletter_interest_group(prefs)
     prefs.inject({}) do |interest_groups, (key, value)|
       mailchimp_name = EVENT_TO_MAILCHIMP_PREF_MAPPINGS[key]
       if mailchimp_name
@@ -79,13 +64,13 @@ class MailchimpWrapper
     end
   end
 
-  def prefs_to_interest_groups_for_categories(categories)
+  def prefs_to_categories_interest_group(categories)
     prefs = assemble_prefs_hash(categories)
     map_category_names_to_ids(prefs)
   end
 
   def assemble_prefs_hash(categories)
-    prefs = categories_list_interest_groups.keys.each_with_object({}) do |category, interest_groups|
+    prefs = users_list_categories_interest_group.keys.each_with_object({}) do |category, interest_groups|
       interest_groups[category] = false
     end
     assign_category_prefs(categories, prefs)
@@ -93,24 +78,24 @@ class MailchimpWrapper
 
   def users_list_interest_groups(category_name = 'Ello Newsletters')
     @interest_groups ||= begin
-                      category_id = users_list.interest_categories.retrieve['categories'].find { |c| c['title'] == category_name }['id']
+                      category_id = retrieve_category_id(category_name)
                       users_list.interest_categories(category_id).interests.retrieve['interests'].map { |g| { name: g['name'], id: g['id'] } }
                     end
   end
 
-  def categories_list_interest_groups
-    id = interest_categories_id_for_categories
-    categories_list.interest_categories(id).interests.retrieve['interests'].each_with_object({}) do |category, categories|
+  def users_list_categories_interest_group
+    id = retrieve_category_id('Categories')
+    users_list.interest_categories(id).interests.retrieve['interests'].each_with_object({}) do |category, categories|
       categories[category['name'].downcase] = category['id']
     end
   end
 
-  def interest_categories_id_for_categories
-    @id ||= categories_list.interest_categories.retrieve['categories'].first['id']
+  def retrieve_category_id(category_name)
+    users_list.interest_categories.retrieve['categories'].detect { |c| c['title'] == category_name }['id']
   end
 
   def map_category_names_to_ids(prefs)
-    category_map = categories_list_interest_groups
+    category_map = users_list_categories_interest_group
     prefs.each_with_object({}) do |(category, value), interest_groups|
       category_id = category_map[category]
       interest_groups[category_id] = value
@@ -120,8 +105,8 @@ class MailchimpWrapper
   def assign_category_prefs(categories, prefs)
     categories.each do |category|
       unless prefs.key?(category)
-        id = interest_categories_id_for_categories
-        categories_list.interest_categories(id).interests.create(body: { name: category.capitalize })
+        id = retrieve_category_id('Categories')
+        users_list.interest_categories(id).interests.create(body: { name: category.capitalize })
       end
       prefs[category] = true
     end
