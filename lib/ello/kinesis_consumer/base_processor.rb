af326e30f9a9b74515f1a41e22c8751135addae4
Jay Zeschin <jay.zeschin@modeset.com>
2016-10-25 16:07:31 -0600

    Allow the Kinesis batch size to be customized with an ENV var

--- lib/ello/kinesis_consumer/base_processor.rb
+++ lib/ello/kinesis_consumer/base_processor.rb
@@ -17,7 +17,8 @@ module Ello
       end
 
       def run!
-        @stream_reader.run! do |record, schema_name|
+        batch_size = Integer(ENV['CONSUMER_BATCH_SIZE'] || StreamReader::DEFAULT_BATCH_SIZE)
+        @stream_reader.run!(batch_size: batch_size) do |record, schema_name|
           @logger.info "#{schema_name}: #{record}"
           method_name = schema_name.underscore
           send method_name, record if respond_to?(method_name)
